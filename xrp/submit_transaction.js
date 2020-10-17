c76d16888254e0e13667bf40616c8463cc9a5d8c
David Simmons <dr.desimmons@gmail.com>
2020-10-17 09:57:59 +0100

    master: adding xrp transaction submission tool

---
+++ xrp/submit_transaction.js
@@ -0,0 +1,24 @@
+var signedTransaction = process.argv.slice(2);
+console.log('signedTransaction: ', signedTransaction[0]);
+
+const RippleAPI = require('ripple-lib').RippleAPI;
+
+const api = new RippleAPI({
+  server: 'wss://s1.ripple.com' // Public rippled server hosted by Ripple, Inc.
+});
+api.on('error', (errorCode, errorMessage) => {
+  console.log(errorCode + ': ' + errorMessage);
+});
+api.on('connected', () => {
+  console.log('connected');
+});
+api.on('disconnected', (code) => {
+  // code - [close code](https://developer.mozilla.org/en-US/docs/Web/API/CloseEvent) sent by the server
+  // will be 1000 if this was normal closure
+  console.log('disconnected, code:', code);
+});
+api.connect().then(() => {
+  return api.submit(signedTransaction[0]) 
+}).then(() => {
+  return api.disconnect();
+}).catch(console.error);
